93917d4cfb18d394c9029ccdf55de4c89617d32b
Nekgate <Nekgate@gmail.com>
2024-05-07 21:27:35 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Function that returns the numbers of
     subscribers of a subreddit passed to it"""
-    
+
     apiUrl = "https://reddit.com/r/{}/about.json".format(subreddit)
     userAgent = "Mozilla/5.0"
-    
+
     response = requests.get(apiUrl, headers={"user-agent": userAgent})
     if not response:
         return 0
